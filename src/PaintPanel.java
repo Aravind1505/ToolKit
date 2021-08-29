1541072811d77aa2e91c9c0fa2b97e3ac4136e0a
Aravind <karavind246815@gmail.com>
2021-08-29 19:21:49 +0530

    Fixed Drawing issue, now it comes continuous and works as intended. Need to add features like stroke size and color changes

--- src/PaintPanel.java
+++ src/PaintPanel.java
@@ -7,11 +7,12 @@ import java.util.ArrayList;
 
 public class PaintPanel extends JPanel implements MouseListener, MouseMotionListener {
 
-    ArrayList<Point> alp;
+    ArrayList<Pointx> alcp, aldp;
 
     PaintPanel(Dimension d)
     {
-        this.alp = new ArrayList<Point>();
+        this.alcp = new ArrayList<Pointx>();
+        this.aldp = new ArrayList<Pointx>();
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
         this.setPreferredSize(d);
@@ -23,22 +24,36 @@ public class PaintPanel extends JPanel implements MouseListener, MouseMotionList
     {
         super.paintComponent(g);
         g.setColor(Color.RED);
-        for(int i =0; i < alp.size(); i++)
-            g.fillOval((int)this.alp.get(i).getX(), (int)this.alp.get(i).getY(), 10, 10);
+        Graphics2D g2d = (Graphics2D) g;
+        for(int i =0; i < this.alcp.size(); i++)
+            g.fillOval((int)this.alcp.get(i).getPoint().getX(), (int)this.alcp.get(i).getPoint().getY(), 5, 5);
+
+        if(aldp.size() > 1) {
+            g2d.setStroke(new BasicStroke(5));
+            for (int i = 0; i < aldp.size() - 1; i++) {
+                if(!(this.aldp.get(i).getStr().equals("End")))
+                    g2d.drawLine((int)this.aldp.get(i).getPoint().getX(), (int)this.aldp.get(i).getPoint().getY(), (int)this.aldp.get(i+1).getPoint().getX(), (int)this.aldp.get(i+1).getPoint().getY());
+            }
+        }
+
     }
 
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
-        this.alp.add(mouseEvent.getPoint());
+        this.alcp.add(new Pointx("Point",mouseEvent.getPoint()));
         this.updateUI();
     }
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
+//        System.out.println("Start : " + mouseEvent.getX() + " " + mouseEvent.getY());
+        this.aldp.add(new Pointx("Start", mouseEvent.getPoint()));
     }
 
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
+//        System.out.println("End : " + mouseEvent.getX() + " " + mouseEvent.getY());
+        this.aldp.add(new Pointx("End", mouseEvent.getPoint()));
     }
 
     @Override
@@ -53,12 +68,33 @@ public class PaintPanel extends JPanel implements MouseListener, MouseMotionList
 
     @Override
     public void mouseDragged(MouseEvent mouseEvent) {
-        this.alp.add(mouseEvent.getPoint());
+        this.aldp.add(new Pointx("Cont", mouseEvent.getPoint()));
+//        System.out.println("Dragged : " + mouseEvent.getX() + " " + mouseEvent.getY());
         this.updateUI();
     }
 
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {
         this.updateUI();
+    }
+}
+
+class Pointx{
+    String str;
+    Point point;
+
+    Pointx(String txt, Point p){
+        this.str = txt;
+        this.point = p;
+    }
+
+    public String getStr()
+    {
+        return this.str;
+    }
+
+    public Point getPoint()
+    {
+        return this.point;
     }
 }
